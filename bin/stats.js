0b6cbd2ccff5cda97a5df89c5be08161a8fd4fd4
Melvin Carvalho <melvincarvalho@gmail.com>
2017-01-11 11:57:32 +0100

    Fix stats.js

--- bin/stats.js
+++ bin/stats.js
@@ -1,35 +1,38 @@
 #!/usr/bin/env node
 
-var bot           = require('../').bot
-var CronJob       = require('cron').CronJob
-var child_process = require('child_process')
-var kue           = require('kue')
-var last          = require('array-last')
-var wc_db         = require('wc_db')
-
-
+var bot = require('../').bot
 var queue = bot.createQueue()
 
-queue.complete( function( err, ids ) {
-  console.log('complete')
-  ids.forEach( function( id ) {
-    console.log(id)
-  })
+queue.complete(function (err, ids) {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log('complete')
+    ids.forEach(function (id) {
+      console.log(id)
+    })
+  }
 })
 
-
-queue.active( function( err, ids ) {
-  console.log('active')
-  ids.forEach( function( id ) {
-    console.log(id)
-  })
+queue.active(function (err, ids) {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log('active')
+    ids.forEach(function (id) {
+      console.log(id)
+    })
+  }
 })
 
-
-queue.inactive( function( err, ids ) {
-  console.log('inactive')
-  ids.forEach( function( id ) {
-    console.log(id)
-  })
-  bot.shutdown()
+queue.inactive(function (err, ids) {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log('inactive')
+    ids.forEach(function (id) {
+      console.log(id)
+    })
+    bot.shutdown()
+  }
 })
