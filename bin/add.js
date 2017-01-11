bf6fbea030b03b244e2dcc8c5dc2aff5240c3894
Melvin Carvalho <melvincarvalho@gmail.com>
2017-01-11 11:47:01 +0100

    Fix add

--- bin/add.js
+++ bin/add.js
@@ -1,29 +1,14 @@
 #!/usr/bin/env node
 
 var bot = require('../').bot
-var kue = require('kue')
 
-// init
-var queue = bot.createQueue()
-var mid      = process.argv[2] || "Z"
-var end      = process.argv[3] || "Z"
-
-
-
-var job = {'11':
-  {
-    "title": '11 unknowns search',
-    "mid": mid,
-    "end" : end
-  }
-}
-
+var job = { '11': { 'title': '11 unknowns search' } }
 
 // main
-bot.addJob(job).then(function(ret){
+bot.addJob(job).then(function (ret) {
   console.log(ret.id)
   bot.shutdown()
-}).catch(function(err){
+}).catch(function (err) {
   console.error(err)
   bot.shutdown()
 })
